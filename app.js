00f686b4ba86afdb0fa22de8d69673f583d58718
Egwali Chukwunalu <chukeg2012@gmail.com>
2021-07-08 14:53:18 +0100

    edit app.js

--- app.js
+++ app.js
@@ -6,30 +6,30 @@ fs = require('fs')
 
 //create server
 server = http.createServer((req, res) =>{
+
 	let filePath = "";
 
     //build a dynamic file path to determine the page to send to brouser
 
-    if(req.url == '/' || '/home') {
-    	filePath = path.join(__dirname, "index.html")
+    if(req.url === '/' || req.url === '/home') {
+    	filePath = path.join(__dirname, "index.html");
     }
-	else if(req.url == "/about" || "/about-us") {
-		filePath = path.join(__dirname, "/about.html")
+	else if(req.url === '/about' || req.url ===  '/about-us') {
+		filePath = path.join(__dirname, 'about.html');
 	}
-	else {
-		filePath = path.join(__dirname, req.url)
+	else if(req.url === '/contact') {
+		filePath = path.join(__dirname, 'contact.html');
 	}
-	//let filePath = path.join(
-	//__dirname, req.url == '/' ||'/home' ? 'index.html' : '/about-us' ? '/about.html' : req.url);
+
 
 //Read content of html page and  send to brouser
 fs.readFile(filePath, "utf-8", (err, content) => {
 	if(err){
 		if(err.code === "ENOENT") {
 			//page not found
-			fs.readFile(path.join(__dirname, errorPage.html), (err, content) => {
-				res.writeHead(404, {"Content-Type" : text/html})
-				res.end(content, utf-8)
+			fs.readFile(path.join(__dirname, 'errorPage.html'), (err, content) => {
+				res.writeHead(404, {"Content-Type" : 'text/html'})
+				res.end(content, 'utf-8')
 			})
 		}
 		else {
@@ -44,8 +44,10 @@ fs.readFile(filePath, "utf-8", (err, content) => {
 		res.end(content, "utf-8")
 	}
 })
-
+console.log(req.url)
+console.log(req.url)
 })
+
 
 PORT = 8000
 server.listen(PORT, () => {
